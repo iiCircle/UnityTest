eed1b3fd570f5f9e5d89b2e27aa0860601d55652
Circle <chentengfei@bilibili.com>
2020-06-03 18:06:04 +0800

    1

---
+++ Assets/Test.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test{
+
+    public static void BuildTest()
+    {
+        Debug.LogError("BuildTest");
+    }
+}
